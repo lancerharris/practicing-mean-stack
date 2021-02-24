182dc244a5631f59f001111b50ac842be9180253
Lance Harris <lancerprogram@gmail.com>
2021-02-24 15:34:59 -0500

    Add fake posts to backend

--- express-backend/app.js
+++ express-backend/app.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  next();
-})
-
-app.use((req, res, next) => {
-  res.send('Hello');
-})
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    { title: "first", content: "first content", id: "1" },
+    { title: "second", content: "second content", id: "2" },
+  ];
+  res.status(200).json({
+    message: 'posts fetched successfully',
+    posts: posts
+  });
+});
 
 module.exports = app;
