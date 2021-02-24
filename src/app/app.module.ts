d6fdec53d7852a2cc596c53ddd60270893634acb
Lance Harris <lancerprogram@gmail.com>
2021-02-24 16:22:10 -0500

    Connect to backend using HTTPClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppComponent } from './app.component';
 import { MatInputModule } from '@angular/material/input';
@@ -25,6 +26,7 @@ import { PostListComponent } from './posts/post-list/post-list.component'
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatFormFieldModule,
