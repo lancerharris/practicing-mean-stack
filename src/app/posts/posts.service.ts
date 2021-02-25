ef935975fd07cf03d608a51d6af81351ac2f9e11
Lance Harris <lancerprogram@gmail.com>
2021-02-25 13:09:25 -0500

    Configure angular to fetch posts from server/mongo.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -14,11 +15,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -29,12 +39,12 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe((resData) => {
         console.log(resData.message);
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
-      })
-
+      });
   }
 }
