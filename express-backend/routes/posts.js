5e8b6f609d5ffa9605ade9dac4b860295b6e0e5c
Lance Harris <lancerprogram@gmail.com>
2021-02-27 16:17:04 -0500

    Add token checking on backend

--- express-backend/routes/posts.js
+++ express-backend/routes/posts.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const router = express.Router();
 
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth")
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 
 router.post(
   "",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
@@ -53,6 +55,7 @@ router.post(
 
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -104,7 +107,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     res.status(200).json({ message: "Post deleted" });
   });
