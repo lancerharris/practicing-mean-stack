c9481c1a67b1483ce5eeab46d8ce340ace648ed2
Lance Harris <lancerprogram@gmail.com>
2021-02-28 09:02:41 -0500

    Improve server auth and add user to post

--- express-backend/middleware/check-auth.js
+++ express-backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.JWT_PASS)
+    const decodedToken = jwt.verify(token, process.env.JWT_PASS); // retrieves the token decoded.
+    req.userData = {email: decodedToken.email, userId: decodedToken.userId}
     next();
   } catch (error) {
     res.status(401).json({message: "auth failed"});
--- express-backend/routes/posts.js
+++ express-backend/routes/posts.js
@@ -37,6 +37,7 @@ router.post(
     const post = new Post({
       title: req.body.title,
       content: req.body.content,
+      creator: req.userData.userId,
       imagePath: url + "/images/" + req.file.filename, // req.file.filename provided by multer
     });
     post.save().then((createdPost) => {
@@ -67,10 +68,15 @@ router.put(
       _id: req.body.id,
       title: req.body.title,
       content: req.body.content,
-      imagePath: imagePath
+      imagePath: imagePath,
+      creator: req.userData.userId
     });
-    Post.updateOne({ _id: req.params.id }, post).then((result) => {
-      res.status(200).json({ message: "update successful" });
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then((result) => {
+      if (result.nModified > 0) {
+        res.status(200).json({ message: "update successful" });
+      } else {
+        res.status(401).json({ message: "Not authorized" });
+      }
     });
   }
 );
@@ -108,8 +114,12 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({ message: "Post deleted" });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId  }).then((result) => {
+    if (result.n > 0) {
+      res.status(200).json({ message: "Post deleted" });
+    } else {
+      res.status(401).json({ message: "Not authorized" });
+    }
   });
 });
 
